ab97e7fe360a80f13b5c568e502ac75179e8a7d0
jlewisit <joelewis.mortgage@gmail.com>
2022-09-21 23:22:28 -0700

    readme update

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 // Connect to database
 const connection = mysql.createConnection({
     host: 'localhost',
-    // port: 3001,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_Name,
